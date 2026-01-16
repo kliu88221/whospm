f998d30d33fcc585f058d0ec3042c98039a0bf33
yu <ylu60@stuy.edu>
2026-01-15 20:16:17 -0500

    i am going to lose it

--- app/static/js/hompage.js
+++ app/static/js/hompage.js
@@ -1,14 +1,14 @@
-const ingredients = ['pepperoni', 'pineapple', 'ham']
-const toppings = {};
+const ingredients = ['Pepperoni', 'Pineapple', 'Ham']
+const ASSETS = {};
 
 
 let loaded = 0
 ingredients.forEach(item => {
   const img = new Image()
-  const path = '/static/img/' + item.toLowerCase() +'.png'
+  const path = '/static/img/' + item.toLowerCase() + '.png';
   img.src = path
   img.onload = () =>{
-    toppings[path] = img
+    ASSETS[path] = img;
     loaded++;
     if(loaded == ingredients.length) {
       render()
@@ -18,14 +18,18 @@ ingredients.forEach(item => {
 
 function render(){
   const containers = document.querySelectorAll('.pizza-container')
-  containers.forEach(c => {
-      const pizza = JSON.parse(c.dataset.pizza);
-      const canvas = container.querySelector('canvas');
-      drawPizza(canvas, pizza);
+  containers.forEach(container => {
+      const data = container.dataset.pizza;
+      if (data) {
+        const pizza = JSON.parse(rawData);
+        const canvas = container.querySelector('canvas');
+        drawPizza(canvas, pizza);
+      }
   })
 }
 
 function drawPizza(canvas, pizza) {
+    if (!canvas) return;
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -39,18 +43,24 @@ function drawPizza(canvas, pizza) {
       ctx.arc(250, 250, 225, Math.PI, -Math.PI)
       ctx.fillStyle = pizza.sauce_color
       ctx.fill()
+    }else{
+      print("where sauce")
     }
 
-    pizza.toppings.forEach(t => {
-      const img = TOPPINGS[t.image_url];
-      if (img) {
-        ctx.drawImage(img, t.x - 37.5, t.y - 37.5, 75, 75);
-      } else {
-        // fall back
-        ctx.fillStyle = "red";
-        ctx.beginPath(); ctx.arc(t.x, t.y, 10, 0, Math.PI*2); ctx.fill();
-      }
-    });
+    if(pizza.toppings){
+      pizza.toppings.forEach(t => {
+        const img = ASSETS[t.image_url];
+        if (img) {
+          ctx.drawImage(img, t.x - 37.5, t.y - 37.5, 75, 75);
+        } else {
+          // fall back
+          ctx.fillStyle = "red";
+          ctx.beginPath(); 
+          ctx.arc(t.x, t.y, 10, 0, Math.PI*2); 
+          ctx.fill();
+        }
+      });
+    }
 
 }
 
